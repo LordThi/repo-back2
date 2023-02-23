0ba090d4a185f9860210b183672a27918c117d47
Geoffroypoupart <geoffroypoupart@gmail.com>
2023-02-23 15:25:08 +0100

    modif file index back

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const logger = require('./app/helpers/logger');
 const router = require('./app/routers');
 
 const port = process.env.PORT || 3000;
@@ -12,5 +13,5 @@ const app = express();
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`Server ready: http://localhost:${port}`);
+  logger.log(`Server ready: http://localhost:${port}`);
 });
