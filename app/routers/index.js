2bb9926cf1e0dfdbc099cbaf4672af6c3b92bff9
Geoffroypoupart <geoffroypoupart@gmail.com>
2023-02-24 16:27:12 +0100

    ajout allergies.md

--- app/routers/index.js
+++ app/routers/index.js
@@ -5,4 +5,17 @@ const router = express.Router();
 
 router.get('/', allintController.homePage);
 
+/*
+ROUTES A CREER
+user (post)
+  user/role
+  user/personal_list
+allergy (post) ?? crréation d'un fichier JSON pour les 14 allergies ou DB interne ou une API ??
+
+lié à une API
+product
+category
+brand
+*/
+
 module.exports = router;
