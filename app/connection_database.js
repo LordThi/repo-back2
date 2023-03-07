2c93dac1496d200a4997b34393eea98975bee80e
Geoffroypoupart <geoffroypoupart@gmail.com>
2023-03-07 11:01:46 +0100

    modified connection database

--- app/connection_database.js
+++ app/connection_database.js
@@ -1,8 +1,8 @@
-const { Client } = require('pg')
+const { Client } = require('pg');
 
 // clients will also use environment variables
 // for connection information
-const client = new Client(process.env.PG_URL);
+const client = new Client(process.env.DATABASE_URL);
 
 client.connect();
 
