7046eb1fbee851e36c3e891805fc97ba9d269e43
Geoffroypoupart <geoffroypoupart@gmail.com>
2023-03-09 15:53:21 +0100

    test

--- app/routers/index.js
+++ app/routers/index.js
@@ -26,5 +26,5 @@ router.post('/allergy/user', isLogger(), allergyController.postForUser); // CREA
 router.delete('/allergy/user', isLogger(), allergyController.deleteByUser); // DELETE
 
 // Search product by product name
-
+//test
 module.exports = router;
