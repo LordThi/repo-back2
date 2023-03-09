06cb0e26f52c3106d7fae04638941a8f8f295a94
Tatiana1981 <tatyumi29@gmail.com>
2023-03-09 13:40:19 +0100

    hotfix: add cors

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('./app/helpers/logger');
 const router = require('./app/routers/index');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
