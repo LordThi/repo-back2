0f656b901193af359e3c7ee546b6d7384405b2f8
Tatiana1981 <tatyumi29@gmail.com>
2023-03-09 14:35:55 +0100

    fixcors vip

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const logger = require('./app/helpers/logger');
 const router = require('./app/routers/index');
-const cors = require('cors');
+
 
 const port = process.env.PORT || 3000;
 
