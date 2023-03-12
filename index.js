1c276c8bdb70823fc4773c3a1fdf15ce8c52d73b
LordThi <92553090+LordThi@users.noreply.github.com>
2023-03-12 14:52:17 +0100

    :bug: cors v5

--- index.js
+++ index.js
@@ -16,16 +16,6 @@ app.use(cors({
   }
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin');
-  next();
-});
-
-// app.use(cors({
-//   credentials: true,
-//   }
-// ));
-
 app.use(express.json());
 
 app.use(router);
